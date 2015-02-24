f2856367ec3a661eaee0cfd6b103712987bf1a94
Josh Curcio <joshua.curcio@cuw.edu>
2015-02-24 14:35:23 -0600

    Added prevNode and tail

---
+++ Driver.java
@@ -0,0 +1,20 @@
+
+public class Driver 
+{
+
+	public static void main(String[] args) 
+	{
+		LinkedList ll = new LinkedList();
+		ll.addFront(2);
+		ll.addFront(3);
+		ll.addFront(4);
+		ll.addFront(5);
+		ll.addFront(6);
+		ll.addFront(7);
+		ll.display();
+		ll.displayInReverse();
+		ll.addAtIndex(13, 2);
+		ll.display();
+		ll.displayInReverse();
+	}
+}
---
+++ Node.java
@@ -0,0 +1,39 @@
+
+public class Node 
+{
+	private Node nextNode;
+	private Node prevNode;
+	private int payload;
+
+	public Node(int payload)
+	{
+		this.payload = payload;
+		this.nextNode = null;
+		this.prevNode = null;
+	}
+
+	public Node getPrevNode() 
+	{
+		return prevNode;
+	}
+
+	public void setPrevNode(Node prevNode) 
+	{
+		this.prevNode = prevNode;
+	}
+	
+	public Node getNextNode() 
+	{
+		return this.nextNode;
+	}
+
+	public void setNextNode(Node nextNode) 
+	{
+		this.nextNode = nextNode;
+	}
+
+	public int getPayload() 
+	{
+		return this.payload;
+	}
+}
